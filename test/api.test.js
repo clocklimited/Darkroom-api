15116e3ef221905869692b21afd23f22a6573cde
Tom Gallacher <tomgallacher23@gmail.com>
2013-02-26 13:10:15 +0000

    Removing unused varibles

--- test/api.test.js
+++ test/api.test.js
@@ -1,11 +1,10 @@
 var darkroom = require('../server')()
   , request = require('supertest')
-  , should = require('should')
 
 
 describe('API', function() {
   it('should not 404 on / route', function(done) {
-    var r = request(darkroom)
+    request(darkroom)
       .get('/')
       .set('Accept', 'application/json')
       .set('X-Appication-Token', 'publicly-viewable')
@@ -17,7 +16,7 @@ describe('API', function() {
 
   describe('#upload', function() {
     it('should upload a single image', function (done) {
-      var r = request(darkroom)
+      request(darkroom)
         .post('/')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
@@ -26,31 +25,12 @@ describe('API', function() {
     })
 
     it('should upload multiple images', function (done) {
-      var r = request(darkroom)
+      request(darkroom)
         .post('/')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
         .end(done)
     })
-
-    // it('should fail on missing API token', function (done) {
-    //   var r = request(darkroom)
-    //     .get('/')
-    //     .set('Accept', 'application/json')
-    //     .expect('Content-Type', /json/)
-    //     .expect(403)
-    //     .end(done)
-    // })
-
-    // it('should fail on unknown API token', function (done) {
-    //   var r = request(darkroom)
-    //     .get('/')
-    //     .set('Accept', 'application/json')
-    //     .expect('Content-Type', /json/)
-    //     .expect(403)
-    //     .end(done)
-    // })
   })
-
 })
