c9aa865fff8276b5e6673db06286164070d479b4
Tom Gallacher <tomgallacher23@gmail.com>
2013-02-26 16:03:57 +0000

    Adding supported image type tests

--- test/resize.test.js
+++ test/resize.test.js
@@ -3,7 +3,48 @@ var darkroom = require('../server')()
   , should = require('should')
 
 
-describe('Resize', function() {
+describe('Resize', function () {
+
+  describe('FileTypes', function () {
+    it('should return a working crop with a png', function(done) {
+      var r = request(darkroom)
+        .post('/resize')
+        .send({ src: 'http://img.clockte.ch/200x200.png?text=Image%20Resize\nTest'
+          , sizes: [100, 100]
+          }
+        )
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (error, res) {
+          if (error) return done(error)
+          res.body.results.should.have.length(1)
+          res.should.be.a('object').and.has.property('100x100')
+          r.app.close()
+          done()
+        })
+    })
+
+    it('should return a working crop with a jpeg', function(done) {
+      var r = request(darkroom)
+        .post('/resize')
+        .send({ src: 'http://img.clockte.ch/200x200.jpg?text=Image%20Resize\nTest'
+          , sizes: [100, 100]
+          }
+        )
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(function (error, res) {
+          if (error) return done(error)
+          res.body.results.should.have.length(1)
+          res.should.be.a('object').and.has.property('100x100')
+          r.app.close()
+          done()
+        })
+    })
+  })
+
   it('should return an object containing a src attribute on post request', function(done) {
     var r = request(darkroom)
       .post('/resize')
