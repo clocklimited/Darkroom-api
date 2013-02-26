7b651dfbf3146170ea2c96f599088e22fe37d9f6
Tom Gallacher <tomgallacher23@gmail.com>
2013-02-26 13:13:55 +0000

    Adding security tests

---
+++ test/security.test.js
@@ -0,0 +1,19 @@
+describe('Api Security', function() {
+  it('should fail on missing API token', function (done) {
+    request(darkroom)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(403)
+      .end(done)
+  })
+
+  it('should fail on unknown API token', function (done) {
+    request(darkroom)
+      .get('/')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(403)
+      .end(done)
+  })
+})
