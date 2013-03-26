a3cdb88e6574fefcc3e0a0bb0b61c82c87cdb8a1
Tom Gallacher <tomgallacher23@gmail.com>
2013-03-26 11:18:55 +0000

    Fixing broken original route

--- endpoint/original.js
+++ endpoint/original.js
@@ -1,11 +1,12 @@
-var fileupload = require('fileupload').createFileUpload(__dirname + '/../images')
+var path = __dirname + '/../images/'
+  , fileupload = require('fileupload').createFileUpload(path)
   , mime = require('mime-magic')
 
 module.exports = function (req, res, next) {
   res.set('X-Application-Method', 'Original Image')
   var file = req.params.data + '/image'
   fileupload.get(file, function(err, data) {
-    mime(__dirname + '/images/' +file, function (err, type) {
+    mime(path + file, function (err, type) {
       if (err) return next(err)
       res.set('Content-Type', type)
       res.write(data)
