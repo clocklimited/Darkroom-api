01319e0cf639f06121dc26fc2b3744e9304b75ac
Tom Gallacher <tomgallacher23@gmail.com>
2013-03-26 21:06:38 +0000

    Fixing broken cluster implementation

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 var createServer = require('./server')
   , config = require('con.figure')(require('./config'))
   , app = createServer()
-  , cluster = require('cluster')
-  , numCPUs = require('os').cpus().length
 
-if (cluster.isMaster) {
-  // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
-    cluster.fork()
-  }
+var clusterMaster = require('./lib/cluster-master')
+clusterMaster(function () {
 
-  cluster.on('exit', function(worker) {
-    console.log('worker ' + worker.process.pid + ' died')
+  var domain = require('domain')
+    , serverDomain = domain.create()
+
+  serverDomain.run(function () {
+    app.listen(process.env.PORT || config.http.port, function () {
+      console.log('%s listening at %s', app.name, app.url)
+    })
   })
-} else {
-  app.listen(config.http.port, function () {
-    console.log('%s listening at %s', app.name, app.url)
-  })
-}
+
+}, { logger: console })
