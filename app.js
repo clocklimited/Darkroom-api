de5dffe54d3ac1ee86e5efc361a1cd9936df84f2
Paul Serby <paul.serby@clock.co.uk>
2015-10-16 16:07:55 +0100

    Ensure setup is via factory

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
 var createServer = require('./server')
   , config = require('con.figure')(require('./config')())
-  , mkdirp = require('mkdirp')
   , clustered = require('clustered')
   , clusterSize = process.env.API_PROCESSES || config.apiProcesses
   , createBackendFactory = require('./lib/backend-factory-creator')
 
-// Ensure dirs are setup
-mkdirp.sync(config.paths.data())
-mkdirp.sync(config.paths.cache())
-
 createBackendFactory(config, function (err, factory) {
 
-  var app = createServer(config, factory)
+  factory.setup(function() {
 
-  if (process.env.NODE_ENV === undefined) {
-    app.log.fatal('NODE_ENV (env) not set, process may crash.')
-  }
+    var app = createServer(config, factory)
 
-  if (isNaN(clusterSize)) {
-    app.log.fatal('Invalid cluster size: "%s", please set/update API_PROCESSES (env) or config.apiProcesses', clusterSize)
-    process.exit(1);
-  }
+    if (process.env.NODE_ENV === undefined) {
+      app.log.fatal('NODE_ENV (env) not set, process may crash.')
+    }
 
-  clustered(function () {
-      var domain = require('domain')
-        , serverDomain = domain.create()
+    if (isNaN(clusterSize)) {
+      app.log.fatal('Invalid cluster size: "%s", please set/update API_PROCESSES (env) or config.apiProcesses', clusterSize)
+      process.exit(1);
+    }
 
-      serverDomain.run(function () {
-        app.listen(process.env.PORT || config.http.port, function () {
-          app.log.info('%s listening at %s', app.name, app.url)
+    clustered(function () {
+        var domain = require('domain')
+          , serverDomain = domain.create()
+
+        serverDomain.run(function () {
+          app.listen(process.env.PORT || config.http.port, function () {
+            app.log.info('%s listening at %s', app.name, app.url)
+          })
         })
-      })
 
-      serverDomain.on('error', function (error) {
-        app.log.error('domain error', error)
-      })
+        serverDomain.on('error', function (error) {
+          app.log.error('domain error', error)
+        })
 
-    }
-  , { logger: app.log
-    , size: clusterSize
-    }
-  )
+      }
+    , { logger: app.log
+      , size: clusterSize
+      }
+    )
 
+  })
 })
