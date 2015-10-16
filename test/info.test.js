f27775831287153bd3a3b55c02559d42cdaf8a99
Paul Serby <paul.serby@clock.co.uk>
2015-10-16 15:55:42 +0100

    Add missing info test

---
+++ test/info.test.js
@@ -0,0 +1,63 @@
+var assert = require('assert')
+  , config = require('con.figure')(require('./config')())
+  , createDarkroom = require('../server')
+  , createBackendFactory = require('../lib/backend-factory-creator')
+  , request = require('supertest')
+  , hashHelper = require('./hash-helper')
+  , async = require('async')
+
+describe('Info', function() {
+  var imgSrcId
+    , darkroom
+    , factory
+
+  before(function (done) {
+    createBackendFactory(config, function (err, backendFactory) {
+      factory = backendFactory
+      darkroom = createDarkroom(config, factory)
+      done()
+    })
+  })
+
+  function clean(done) {
+    async.series([ factory.clean, factory.setup ], done)
+  }
+
+  before(clean)
+  after(clean)
+
+  before(function (done) {
+    request(darkroom)
+      .post('/')
+      .set('x-darkroom-key', 'key')
+      .set('Accept', 'application/json')
+      .attach('file', 'test/fixtures/jpeg.jpeg')
+      .end(function (err, res) {
+        imgSrcId = res.body.src
+        done()
+      })
+  })
+
+  it('should return info from an existing image', function(done) {
+    var uri = '/info/' + imgSrcId
+      , url = uri + ':' + hashHelper(uri)
+    request(darkroom)
+      .get(url)
+      .expect(200)
+      .end(function (error, res) {
+        if (error) return done(error)
+        assert.equal(res.text, '{"width":500,"height":375}')
+        done()
+      })
+  })
+
+  it('should 404 if image is not found', function(done) {
+    var uri = '/info/wang'
+      , url = uri + ':' + hashHelper(uri)
+    request(darkroom)
+      .get(url)
+      .expect(404)
+      .end(done)
+  })
+
+})
