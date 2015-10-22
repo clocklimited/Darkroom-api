0d8a3ae98f9158d2b48fcb9599f1ebceead4ee0a
Paul Serby <paul.serby@clock.co.uk>
2015-10-22 13:09:57 +0100

    Mark mongo write types

--- test/lib/backends/mongo.test.js
+++ test/lib/backends/mongo.test.js
@@ -1,5 +1,6 @@
 var createBackend = require('../../../lib/backends/mongo')
   , tests = require('./backend-tests')
+  , assert = require('assert')
 
 function getConfig() {
   return { databaseUri: 'mongodb://localhost:27017/darkroom-test' }
@@ -7,4 +8,34 @@ function getConfig() {
 
 describe('Mongo Backend', function () {
   tests(createBackend, getConfig)
+
+  describe('meta data', function () {
+    it('should mark data with a `data` type', function (done) {
+      createBackend(getConfig(), function (err, factory) {
+        var writeStream = factory.createDataWriteStream()
+        writeStream.on('done', function (file) {
+          factory._db.collection('fs.files').findOne({ md5: file.id }, function (err, data) {
+            assert.equal(data.metadata.type, 'data')
+            done()
+          })
+        })
+        writeStream.write('hello')
+        writeStream.end()
+      })
+    })
+
+    it('should mark cache with a `cache` type', function (done) {
+      createBackend(getConfig(), function (err, factory) {
+        var writeStream = factory.createCacheWriteStream('meta-test')
+        writeStream.on('done', function (file) {
+          factory._db.collection('fs.files').findOne({ filename: file.id }, function (err, data) {
+            assert.equal(data.metadata.type, 'cache')
+            done()
+          })
+        })
+        writeStream.write('hello')
+        writeStream.end()
+      })
+    })
+  })
 })
