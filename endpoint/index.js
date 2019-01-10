a5a7cbe94ce388743b1720ae12db476032db7951
Adam Duncan <adam.duncan@clock.co.uk>
2019-01-10 17:15:20 +0000

    Implement ability to force a file to download via a /download endpoint

--- endpoint/index.js
+++ endpoint/index.js
@@ -3,6 +3,7 @@ module.exports = function (config, backendFactory) {
     resize: require('./resize')(config, backendFactory)
   , crop: require('./crop')(config, backendFactory)
   , original: require('./original')(config, backendFactory)
+  , download: require('./original')(config, backendFactory, { download: true })
   , info: require('./info')(config, backendFactory)
   , watermark: require('./watermark')(config, backendFactory)
   , upload: require('./upload')
