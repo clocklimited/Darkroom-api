b98e3f4e98b2cea5c3c12c3e4a15b24e6f23565a
Jack Burgess <jackburgess124@gmail.com>
2021-05-12 18:42:24 +0100

    chore: fix benchmark upload

--- benchmark/upload.js
+++ benchmark/upload.js
@@ -1,12 +1,13 @@
 const request = require('request')
 const fs = require('fs')
+const { key } = require('../locations.js')
 
 function createTests(options) {
   function test(cb) {
     request.post(
       {
         url: options.url,
-        headers: { 'x-darkroom-key': 'keyey-key' },
+        headers: { 'x-darkroom-key': key },
         formData: {
           file: fs.createReadStream('/dev/urandom', {
             start: 1,
