ade7c242386739b86b21731ebc8b2f90fa00863a
rymate1234 <supermarioryan@gmail.com>
2021-05-14 22:26:46 +0100

    > refactor / wip tests

--- endpoint/blur.js
+++ endpoint/blur.js
@@ -1,28 +1,15 @@
 const darkroom = require('@clocklimited/darkroom')
-const url = require('url')
 const restifyErrors = require('restify-errors')
 
 module.exports = function (serviceLocator, backendFactory) {
   const { logger } = serviceLocator
   return async function (req, res, next) {
     res.set('X-Application-Method', 'User defined image blur')
-    if (typeof req.body === 'string') req.body = JSON.parse(req.body)
     let { src, masks, method } = req.body
-
-    const srcUrl = url.parse(src).path.split('/')
-    req.params.data = srcUrl[srcUrl.length - 1]
-
-    if (masks === undefined) {
-      return next(new restifyErrors.BadDigestError('Masks are undefined'))
-    }
 
     logger.info(
       { id: req.requestId },
-      'Blur Request made for image: ' +
-        req.params.data +
-        ' with ' +
-        masks.length +
-        ' masks'
+      'Blur Request made for image: ' + src + ' with ' + masks.length + ' masks'
     )
     logger.info(
       { id: req.requestId },
@@ -30,62 +17,62 @@ module.exports = function (serviceLocator, backendFactory) {
     )
 
     const collection = {}
+    let error = false
 
-    try {
-      await new Promise((resolve, reject) => {
-        logger.info({ id: req.requestId }, 'Creating blurred ')
-        const store = backendFactory.createDataWriteStream()
-        const crop = new darkroom.Blur({ masks, method })
-
-        store.once('error', function (error) {
-          logger.error({ id: req.requestId }, 'StoreStream:', error)
-          reject(error)
-        })
-
-        // changed from “once” to “on” because with “once” the server would crash on an error
-        crop.on('error', function (error) {
-          logger.error(
-            { id: req.requestId },
-            'Blur',
-            error.toString ? error.toString() : error
-          )
-          reject(error)
-        })
-
-        store.once('done', function (file) {
-          const values = []
-          let key = null
-          for (const mask of masks) {
-            values.push(mask.map(([x, y]) => `${x},${y}`).join(' '))
-          }
-
-          key = values.join(':')
-          collection.id = file.id
-          collection.key = key
-          logger.info(
-            { id: req.requestId },
-            'Successfully created blur with ' +
-              values.length +
-              ' areas: ' +
-              file.id
-          )
-          resolve()
-        })
-        const readStream = backendFactory.createDataReadStream(req.params.data)
-
-        readStream.once('notFound', () => {
-          next(new restifyErrors.ResourceNotFoundError('Not Found'))
-        })
-
-        readStream.pipe(crop).pipe(store, {
-          gravity: req.params.gravity,
-          format: req.params.format
-        })
-      })
-    } catch (error) {
-      logger.error({ id: req.requestId }, error)
-      return next(new restifyErrors.BadDigestError(error.message))
+    const processError = (err) => {
+      if (error) return
+      error = true
+      return next(new restifyErrors.BadDigestError(err.message))
     }
-    res.json(collection)
+
+    logger.info({ id: req.requestId }, 'Creating blurred ')
+    const store = backendFactory.createDataWriteStream()
+    const blur = new darkroom.Blur({ masks, method })
+
+    store.once('error', function (error) {
+      logger.error({ id: req.requestId }, 'StoreStream:', error)
+      processError(error)
+    })
+
+    // changed from “once” to “on” because with “once” the server would crash on an error
+    blur.on('error', function (error) {
+      logger.error(
+        { id: req.requestId },
+        'Blur',
+        error.toString ? error.toString() : error
+      )
+      processError(error)
+    })
+
+    store.once('done', function (file) {
+      const values = []
+      let key = null
+      for (const mask of masks) {
+        values.push(mask.map(([x, y]) => `${x},${y}`).join(' '))
+      }
+
+      key = values.join(':')
+      collection.id = file.id
+      collection.src = src
+      collection.key = key
+      logger.info(
+        { id: req.requestId },
+        'Successfully created blur with ' + values.length + ' areas: ' + file.id
+      )
+      if (!error) {
+        res.json(collection)
+      }
+    })
+    const readStream = backendFactory.createDataReadStream(src)
+
+    readStream.once('notFound', () => {
+      error = true
+      next(new restifyErrors.ResourceNotFoundError('Not Found'))
+    })
+
+    readStream.pipe(blur).pipe(store, {
+      gravity: req.params.gravity,
+      format: req.params.format
+    })
   }
 }
---
+++ test/blur.test.js
@@ -0,0 +1,122 @@
+const mockServiceLocator = require('./mock-service-locator')
+const createDarkroom = require('../server')
+const createBackendFactory = require('../lib/backend-factory-creator')
+const request = require('supertest')
+const path = '/blur'
+const assert = require('assert')
+const backends = require('./lib/backends')
+const async = require('async')
+
+backends().forEach(function (backend) {
+  var config = backend.config
+
+  describe('Blur ' + backend.name + ' backend', function () {
+    var jpegId, pngId, darkroom, factory
+
+    before(function (done) {
+      const sl = mockServiceLocator(config)
+      createBackendFactory(sl, function (err, backendFactory) {
+        factory = backendFactory
+        darkroom = createDarkroom(sl, factory)
+        done()
+      })
+    })
+
+    before((done) => factory.setup(done))
+    after((done) => factory.clean(done))
+
+    before(function (done) {
+      async.parallel(
+        [
+          (next) =>
+            request(darkroom)
+              .post('/')
+              .set('x-darkroom-key', 'key')
+              .set('Accept', 'application/json')
+              .attach('file', 'test/fixtures/jpeg.jpeg')
+              .end(function (err, res) {
+                jpegId = res.body.id
+                next()
+              }),
+          (next) =>
+            request(darkroom)
+              .post('/')
+              .set('x-darkroom-key', 'key')
+              .set('Accept', 'application/json')
+              .attach('file', 'test/fixtures/png.png')
+              .end(function (err, res) {
+                pngId = res.body.id
+                next()
+              })
+        ],
+        done
+      )
+    })
+
+    describe('FileTypes', function () {
+      it('should return a working blur with a png', function (done) {
+        request(darkroom)
+          .post(path)
+          .send({
+            src: pngId,
+            masks: [
+              [
+                [0, 100],
+                [100, 100],
+                [100, 0]
+              ]
+            ]
+          })
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (error, res) {
+            if (error) return done(error)
+            assert.strictEqual(Object.keys(res.body).length, 3)
+            assert(res.body instanceof Object)
+            assert.deepStrictEqual(res.body.src, jpegId)
+            assert.doesNotMatch(res.body.id, jpegId)
+            assert.doesNotMatch(res.body.id, undefined)
+            done()
+          })
+      })
+
+      it('should return a working crop with a jpeg', function (done) {
+        request(darkroom)
+          .post(path)
+          .send({
+            src: jpegId,
+            masks: [
+              [
+                [0, 100],
+                [100, 100],
+                [100, 0]
+              ]
+            ]
+          })
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end(function (error, res) {
+            if (error) return done(error)
+            assert.strictEqual(Object.keys(res.body).length, 3)
+            assert(res.body instanceof Object)
+            assert.deepStrictEqual(res.body.src, jpegId)
+            assert.doesNotMatch(res.body.id, jpegId)
+            assert.doesNotMatch(res.body.id, undefined)
+            done()
+          })
+      })
+    })
+
+    it('should succeed with src only', function (done) {
+      request(darkroom)
+        .post(path)
+        .send({ src: '3bec4be4b95328cb281a47429c8aed8e' })
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end(done)
+    })
+  })
+})
